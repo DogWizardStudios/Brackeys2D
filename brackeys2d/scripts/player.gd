78f81c0565706402ad0d01d1a55c034f51a0ab90
DogWizardStudios <DogWizardStudios@gmail.com>
2025-04-28 19:40:36 +0100

    Update player movement

--- brackeys2d/scripts/player.gd
+++ brackeys2d/scripts/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -11,12 +13,29 @@ func _physics_process(delta):
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	#play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle") #on floor and not moving = idle
+		else:
+			animated_sprite.play("run") #on floor and moving = run
+	else:
+		animated_sprite.play("jump") # not on floor = jumping/falling
+
+	
+	#applies movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
